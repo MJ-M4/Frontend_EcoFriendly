b3de5b40ad2379bfbf5cf607142efd703a4c40ee
MJ-M4 <mohmdemad2040@gmail.com>
2025-02-10 20:18:20 +0200

    Initial commit

---
+++ frontend/src/components/ContainerList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ContainerList = () => {
+    const [containers, setContainers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/containers')
+             .then(response => setContainers(response.data))
+             .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Garbage Containers</h2>
+            <ul>
+                {containers.map(container => (
+                    <li key={container.id}>
+                        {container.location} - {container.fullness_level}% full
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ContainerList;

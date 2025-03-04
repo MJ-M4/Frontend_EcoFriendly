9c69163238bd0342c5bd370ade27da3338f9f5b5
eggsclusive <yona.mattar@gmail.com>
2025-03-04 22:31:43 +0200

    Initial commit

--- frontend/src/components/General.js
+++ frontend/src/components/General.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
     { id: 'bin_5', status: 'Full', capacity: 50, time: '18:30', date: '15-1-2025', route: 'Route E', battery: 78, lat: 32.7120, lon: 35.3245 },
   ];
 
-  const user = { name: 'Sami', avatar: '/images/sami.png' };
+  const user = { name: 'Mohamed Mhagne', avatar: '/images/sami.png' };
 
   const handleSelectBin = (bin) => {
     setSelectedBin(bin);

8492e3056eb0100072704294092c769b1d036392
MJ-M4 <mohmdemad2040@gmail.com>
2025-03-04 06:17:33 +0200

    Test map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
+import BinLocations from './components/BinLocations';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/bins" element={<BinLocations />} />
       </Routes>
     </Router>
   );
